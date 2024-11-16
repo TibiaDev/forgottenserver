28c5ae1b348ad99cec8ad2cd9e9b42b823ef9ad6
Felipe Muniz <felipemuniznet@gmail.com>
2024-11-16 02:04:39 -0300

    feat: revert world time implementation, adjustments on motd on schema.sql

--- src/luascript.cpp
+++ src/luascript.cpp
@@ -1217,15 +1217,6 @@ void LuaScriptInterface::registerFunctions()
 	// getDepotId(uid)
 	lua_register(L, "getDepotId", LuaScriptInterface::luaGetDepotId);
 
-	// getWorldTime()
-	lua_register(L, "getWorldTime", LuaScriptInterface::luaGetWorldTime);
-
-	// getWorldLight()
-	lua_register(L, "getWorldLight", LuaScriptInterface::luaGetWorldLight);
-
-	// setWorldLight(level, color)
-	lua_register(L, "setWorldLight", LuaScriptInterface::luaSetWorldLight);
-
 	// getWorldUpTime()
 	lua_register(L, "getWorldUpTime", LuaScriptInterface::luaGetWorldUpTime);
 
@@ -3519,39 +3510,6 @@ int LuaScriptInterface::luaDebugPrint(lua_State* L)
 	// debugPrint(text)
 	reportErrorFunc(L, tfs::lua::getString(L, -1));
 	return 0;
-}
-
-int LuaScriptInterface::luaGetWorldTime(lua_State* L)
-{
-	// getWorldTime()
-	int16_t time = g_game.getWorldTime();
-	lua_pushnumber(L, time);
-	return 1;
-}
-
-int LuaScriptInterface::luaGetWorldLight(lua_State* L)
-{
-	// getWorldLight()
-	LightInfo lightInfo = g_game.getWorldLightInfo();
-	lua_pushnumber(L, lightInfo.level);
-	lua_pushnumber(L, lightInfo.color);
-	return 2;
-}
-
-int LuaScriptInterface::luaSetWorldLight(lua_State* L)
-{
-	// setWorldLight(level, color)
-	if (ConfigManager::getBoolean(ConfigManager::DEFAULT_WORLD_LIGHT)) {
-		tfs::lua::pushBoolean(L, false);
-		return 1;
-	}
-
-	LightInfo lightInfo;
-	lightInfo.level = tfs::lua::getNumber<uint8_t>(L, 1);
-	lightInfo.color = tfs::lua::getNumber<uint8_t>(L, 2);
-	g_game.setWorldLightInfo(lightInfo);
-	tfs::lua::pushBoolean(L, true);
-	return 1;
 }
 
 int LuaScriptInterface::luaGetWorldUpTime(lua_State* L)
